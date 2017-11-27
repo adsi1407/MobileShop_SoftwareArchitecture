b9b7b4989360ab0f11e9f3272ea7bc75992656f1
David Santacoloma <adsi1409@hotmail.com>
2017-11-27 02:00:27 -0500

    Gral: Agregar paquetes de Xamarin y MvvMCross al proyecto. Eliminar archivos no utilizados. Agregar activities y layout vacíos para login y registro

---
+++ MobileShop/MobileShop.Droid/Views/LoginView.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace MobileShop.Droid.Views
+{
+    [Activity(MainLauncher = true)]
+    public class LoginView : MvxAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.login_activity);
+        }
+    }
+}
---
+++ MobileShop/MobileShop.Droid/Views/SignUpView.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using MobileShop.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.AppCompat;
+
+namespace MobileShop.Droid.Views
+{
+    [Activity]
+    public class SignUpView : MvxAppCompatActivity
+    {
+        #region Properties
+
+        public new LoginViewModel ViewModel
+        {
+            get { return (LoginViewModel)base.ViewModel; }
+            set { base.ViewModel = value; }
+        }
+
+        #endregion
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.signup_activity);
+        }
+    }
+}
