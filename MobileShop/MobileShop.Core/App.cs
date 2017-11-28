da5b7e20807a1ce31080d501a138c2762cd0cc2e
David Santacoloma <adsi1409@hotmail.com>
2017-11-27 23:54:39 -0500

    Agregar clase de entrada para el Core

---
+++ MobileShop/MobileShop.Core/App.cs
@@ -0,0 +1,13 @@
+﻿using MobileShop.Core.ViewModels;
+using MvvmCross.Core.ViewModels;
+
+namespace MobileShop.Core
+{
+    public class App: MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterNavigationServiceAppStart<LoginViewModel>();
+        }
+    }
+}
