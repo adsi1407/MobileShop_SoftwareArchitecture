84dea0f6918acc19decd35c1eddd6ce61a2f7358
David Santacoloma <adsi1409@hotmail.com>
2017-11-28 08:05:34 -0500

    Implementar repository de cuenta para el registro y login

--- MobileShop/MobileShop.Repository/AccountRepository.cs
+++ MobileShop/MobileShop.Repository/AccountRepository.cs
@@ -2,19 +2,29 @@
 using System.Threading.Tasks;
 using MobileShop.Domain.Dtos;
 using MobileShop.Domain.Repositories;
+using System.Net.Http;
 
 namespace MobileShop.Repository
 {
     public class AccountRepository : IAccountRepository
     {
-        public Task<bool> Login(User user)
+        private const string registerEndpoint = "api/Account/Register";
+        private const string loginEndpoint = "api/Account/Login";
+        private const string urlBase = "";
+        private string fullUrl;
+
+        public async Task<bool> Login(User user)
         {
-            throw new NotImplementedException();
+            fullUrl = string.Format("{0}{1}", urlBase, loginEndpoint);
+            var repositoryBase = new RepositoryBaseRest<User, bool>();
+            return await repositoryBase.ConsumeRestService(user, fullUrl, HttpMethod.Post);
         }
 
-        public Task<bool> Register(Persona persona)
+        public async Task<bool> Register(Persona persona)
         {
-            throw new NotImplementedException();
+            fullUrl = string.Format("{0}{1}", urlBase, registerEndpoint);
+            var repositoryBase = new RepositoryBaseRest<Persona, bool>();
+            return await repositoryBase.ConsumeRestService(persona, fullUrl, HttpMethod.Post);
         }
     }
 }
